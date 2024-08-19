38edb7334b252906c55eb50b5ff2e90b9900440c
Abhishek20110 <71166991+Abhishek20110@users.noreply.github.com>
2024-08-19 13:31:07 +0530

    Add files via upload

--- routes/userActionRoutes.js
+++ routes/userActionRoutes.js
@@ -14,11 +14,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const action = express.Router();
-const uploadfm = multer();
-,
 
 // Update user
-action.put('/update',uploadfm.none(), userAuth, async (req, res) => {
+action.put('/update', userAuth, async (req, res) => {
     try {
         const { name, email, phone, zip } = req.body;
         const { userId } = req.user;
@@ -68,7 +66,7 @@ action.put('/update',uploadfm.none(), userAuth, async (req, res) => {
 });
 
 // Update password
-action.put('/updatepass',uploadfm.none(), userAuth, async (req, res) => {
+action.put('/updatepass', userAuth, async (req, res) => {
     try {
         const { currentPassword, newPassword, conPassword } = req.body;
         const { userId } = req.user;
