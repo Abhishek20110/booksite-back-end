dae726907c54bc0b917be8588ff098cc045af1ab
Abhishek20110 <71166991+Abhishek20110@users.noreply.github.com>
2024-08-22 12:02:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors'; // Import CORS middleware
+import cors from 'cors';
 import db from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import action from './routes/userActionRoutes.js';
 import BookAction from './routes/BookRoutes.js';
 import colors from 'colors';
+import { v2 as cloudinary } from 'cloudinary';
 
 dotenv.config();
 db();
 
+// Debugging: Check if environment variables are loaded correctly
+console.log("Cloudinary Name:", process.env.CLOUDINARY_NAME);
+console.log("Cloudinary API Key:", process.env.CLOUDINARY_API_KEY);
+console.log("Cloudinary API Secret:", process.env.CLOUDINARY_API_SECRET);
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
 
 // Middleware
-app.use(express.json()); // Built-in body parser for JSON
-app.use(express.urlencoded({ extended: true })); // Built-in body parser for URL-encoded data
-app.use(cors()); // Enable CORS for all routes (you can specify origins if needed)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // Root route to show a welcome message
 app.get('/', (req, res) => {
